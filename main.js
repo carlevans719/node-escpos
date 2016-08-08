afe5fe51cc47bac5854e757c567ce84a9ecc18d2
Carl Evans <carl@tigatech.co.uk>
2016-08-08 12:37:01 +0100

    Extends commands to include most of those available

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { Printer, PrintJob } = require( './escpos.js' );
+const Pixels = require( 'get-pixels' );
 
 let myPrinter = new Printer();
 let myPrintJob = new PrintJob();
@@ -6,26 +7,49 @@ let myPrintJob = new PrintJob();
 myPrinter.connect();
 
 
-// Make a pretty page...
-myPrintJob.setTextFormat( 'quad' ); // TODO: setTextFormat doesn't work
-// myPrintJob.pad( 1 ); // add some padding
-myPrintJob.text( 'This is line 1' ); // add some text
+// // Make a pretty page...
+// myPrintJob.setRotate( true );
+// myPrintJob.text( '|||||||||||||' );
+// // myPrintJob.newLine( 2 );
 //
-// myPrintJob.setTextAlignment( 'center' ); // change the text alignment
-// myPrintJob.separator(); // draw a horizontal line
-//
-// myPrintJob.setTextAlignment( 'right' );
+
+// myPrintJob.setLeftMargin( 10 );
+// myPrintJob.text( 'Oh, what fun this is. I want to check what happens when we need two lines for all the text.' )
+// myPrintJob.newLine();
+// myPrintJob.text( 'Look! Some more text for your reading leisure... Lovely.' )
+// myPrintJob.pad()
+// myPrintJob.cut();
+
+// myPrintJob.setInverted( true );
 // myPrintJob.text( 'This is line 2' );
-//
-// myPrintJob.setTextAlignment( 'center' );
-// myPrintJob.separator();
+// myPrintJob.setInverted( false );
 //
 // myPrintJob.text( 'And line 3' );
-
-// myPrintJob.pad( 1 );
-myPrintJob.cut(); // slice dat.
-
-// Send the printJob to the printer
-myPrinter.print( myPrintJob, function () {
-	console.log( "It's finished printing!!" );
+//
+Pixels( '../../Pictures/Untitled.png', ( err, res ) => {
+	res.data1 = [];
+	res.data2 = [];
+	res.data.forEach( pxl => { res.data2.push( pxl ) } );
+	while ( res.data2.length ) {
+		res.data1.push( res.data2.shift() );
+		res.data2.shift();
+		res.data2.shift();
+		res.data2.shift();
+	}
+	// res[ 0 ].data1 = [];
+	// res[ 0 ].data.forEach( function ( elem ) {
+	// 	res[ 0 ].data1.push( elem );
+	// } );
+	// myPrintJob.cut(); // slice dat.
+	// myPrinter.print( new Buffer( [ 0x1d, 0x2f, 0x00 ] ) );
+	// myPrinter.print( [ new Buffer( [ 0x1d, 0x2a, res[ 0 ].width * 4, res[ 0 ].height ].concat( res[ 0 ].data ) ) ] );
+	// console.log( Array.isArray( res[ 0 ].data1 ) );
+	myPrinter.print( [ new Buffer( [ 0x1d, 0x76, 0x30, 0x03, 0x00, res.shape[ 0 ], 0x00, res.shape[ 1 ] ].concat( res.data1 ) ) ] );
 } );
+
+// myPrintJob.nv();
+//
+// Send the printJob to the printer
+// myPrinter.print( myPrintJob, function () {
+// 	console.log( "It's finished printing!!" );
+// } );
